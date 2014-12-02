08cf658bbac609a0635533f8a6a93d504516ac01
WuNan <geek5nan@gmail.com>
2014-12-02 21:56:38 +0800

    设置忽略李彪

--- OCDemo/ViewController.m
+++ OCDemo/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <pop/POP.h>
 @interface ViewController ()
 
 @end
@@ -17,13 +17,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    
 }
 
 @end
